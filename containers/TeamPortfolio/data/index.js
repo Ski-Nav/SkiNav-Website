4cc567b43e137c21a5f78afc82a582427ce2fb08
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-06-05 23:27:30 -0700

    Adding pictures

---
+++ containers/TeamPortfolio/data/index.js
@@ -0,0 +1,30 @@
+/* ------------------------------------ */
+// Team Portfolio data section
+/* ------------------------------------ */
+import member1 from 'common/assets/image/kyle.jpg';
+import member2 from 'common/assets/image/brian.jpg';
+import member3 from 'common/assets/image/troy.png';
+
+export const teamportfolio = {
+  title: 'Meet our awesome team members, work behind the sense',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit sed eiusmod tempor incididunt labore dolore features that Lorem ipsum dolor sit amet consectetur.',
+
+  teammember: [
+    {
+      id: 1,
+      img: member1,
+      text: 'Kyle',
+    },
+    {
+      id: 2,
+      img: member2,
+      text: 'Brian',
+    },
+    {
+      id: 3,
+      img: member3,
+      text: 'Troy',
+    },
+  ],
+};
